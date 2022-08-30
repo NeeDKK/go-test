00d0a790c5f33527c8d3793277fc6320dafd1487
wuzijie <wuzijie@wangtengtech.com>
2022-08-30 16:08:44 +0800

    添加colly的demo

--- go.mod
+++ go.mod
@@ -5,16 +5,25 @@ go 1.16
 require (
 	baliance.com/gooxml v1.0.1 // indirect
 	github.com/ErmaiSoft/GoOpenXml v0.0.0-20210204025835-ab4edcaefc12 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.5 // indirect
+	github.com/antchfx/xmlquery v1.3.12 // indirect
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/theSmallwhiteMe/make_table_struct v0.0.0-20211014032950-5775508e1228
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220307174427-659dce7fcb03 // indirect
 	google.golang.org/grpc v1.44.0
-	google.golang.org/protobuf v1.28.0
+	google.golang.org/protobuf v1.28.1
 	gorm.io/driver/mysql v1.3.5
 	gorm.io/gorm v1.23.8
 )
--- testString/main.go
+++ testString/main.go
@@ -1,58 +1,113 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"math"
+	"strconv"
+	"strings"
 )
 
-type AllowListModbus struct {
-	AllowListGroupId int    `json:"allowListGroupId"`
-	DeviceName       string `json:"deviceName"`
-	SrcAndDestIp     string `json:"srcAndDestIp"`
-	SrcAndDestMac    string `json:"srcAndDestMac"`
-	Func             string `json:"func"`
-	StartAddr        string `json:"startAddr"`
-	EndAddr          string `json:"endAddr"`
-	Subfunc          string `json:"subfunc"`
+type Test1 interface {
+	Func1()
+}
+type People struct {
+	Name string
+}
+
+type Test2 interface {
+	Func2()
 }
 
 func main() {
-	//resp, err := http.Get("https://10.25.10.120:1443/state/getRegisterInfo")
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//fmt.Println(string(body))
-	certPool := x509.NewCertPool()           // 初始化证书池
-	caCrt, err := ioutil.ReadFile("pub.crt") // 读取ca.crt证书内容
-	if err != nil {
-		log.Fatalln("ca.crt read error:", err)
+	a := "SN=4ZxQX80s3qkHllngZ52GmOHSAbfb0csODz89ZsSvvC+X45o01bjAT3Lex0kLzfwgBCCLFxxFIck=#prod=IPCDefSys;project=IPCDefSys001;pid=;vid=;sku=;KillTool=1;ExternalDevice=1;Whitelist=1;AppGuard=1;apply_date=1659057092;expire_date=1690593092;effective_time=31536000;type=2;uuid=c9f97248-d887-4252-a5e9-7bf1518f1acf;#signature=cbf219979b0b7d96ff9430b717e61f86440fd25a1ea277a71244f6d817d2bd7d"
+	split := strings.Split(a, "#")
+	requestCode := strings.Split(split[0], "SN=")[1]
+	typeString := strings.Split(split[1], "type=")[1]
+	typeString = typeString[:1]
+	num := ""
+	if strings.Contains(split[1], "Whitelist=1;") {
+		num += "1"
+	} else {
+		num += "0"
 	}
-	certPool.AppendCertsFromPEM(caCrt) // 解析ca证书，并添加到证书池
-	// 解析颁发的客户端证书，LoadX509KeyPair由于没有提供解析时传入密码，所以客户端私钥不能有密码，
-	// 否则会报错: tls: failed to parse private key
-	clientCrt, err := tls.LoadX509KeyPair("pub.crt", "key.pem")
-	if err != nil {
-		log.Fatalln("client.crt LoadX509KeyPair error:", err)
+	if strings.Contains(split[1], "ExternalDevice=1;") {
+		num += "1"
+	} else {
+		num += "0"
 	}
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			RootCAs:            certPool,                     // 设置双向认证ca证书
-			Certificates:       []tls.Certificate{clientCrt}, // 客户端证书，需要传递给服务端
-			InsecureSkipVerify: true,                         // 控制客户端是否验证服务器端的证书链和主机名，如果为true则客户端不验证服务器端的证书链和主机名
-		},
+	if strings.Contains(split[1], "KillTool=1;") {
+		num += "1"
+	} else {
+		num += "0"
 	}
-	// 如果InsecureSkipVerify为false，则可能会存在一个报错：x509: certificate is not valid for any names, but wanted to match www.api_domain.com
-	client := &http.Client{Transport: transport}
-	resp, err := client.Get("https://10.25.10.120:1443/state/getRegisterInfo")
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	if strings.Contains(split[1], "AppGuard=1;") {
+		num += "1"
+	} else {
+		num += "0"
+	}
+	fmt.Println(requestCode)
+	fmt.Println(typeString)
+	fmt.Println(num)
+	c := getInput("0100")
+	out := sq(c)
+	sum := 0
+	for o := range out {
+		// fmt.Println(o)
+		sum += o
+	}
+	timeStr := strings.Split(split[1], "effective_time=")[1]
+	timeUsestr, _ := strconv.Atoi(strings.Split(timeStr, ";")[0])
+	timeUse := timeUsestr / 3600 / 24
+	commondstr := "/go/src/netvine/server/generate " + requestCode + " " + typeString + " " + strconv.Itoa(sum) + " " + strconv.Itoa(timeUse)
+	fmt.Println(sum)
+	fmt.Println(commondstr)
+}
+
+func StringToIntArray(input string) []int {
+	output := []int{}
+	for _, v := range input {
+		output = append(output, int(v))
+	}
+	for i, j := 0, len(output)-1; i < j; i, j = i+1, j-1 {
+		output[i], output[j] = output[j], output[i]
+	}
+	return output
+}
+
+func getInput(input string) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, b := range StringToIntArray(input) {
+			out <- b
+		}
+		close(out)
+	}()
+
+	return out
+}
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	var base, i float64 = 2, 0
+	go func() {
+		for n := range in {
+			out <- (n - 48) * int(math.Pow(base, i))
+			i++
+		}
+		close(out)
+	}()
+	return out
+}
+
+func Test(interface{}) {
+	fmt.Println("Test========interface")
+
+}
+
+func (a People) Func1() {
+	fmt.Println("Func1========people")
+}
+
+func (a People) Func2() {
+	fmt.Println("Func2========people")
+
 }
